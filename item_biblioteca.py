a0c70e5a613e24a8e97f86ddd718534ea4e7411b
Felipe Cordeiro <olifelipecordeiro@gmail.com>
2026-03-04 09:56:50 -0300

    Adicionando os atributos da Classe ItemBiblioteca - Utilizando @property (decorador)

--- item_biblioteca.py
+++ item_biblioteca.py
@@ -4,3 +4,33 @@ class ItemBiblioteca:
         self.__titulo = titulo
         self.__ano = ano
         self.__disponivel = True
+
+    @property
+    def codigo(self):
+        return self.__codigo
+
+    @property
+    def titulo(self):
+        return self.__titulo
+
+    @titulo.setter
+    def titulo(self, valor):
+        if valor and valor.strip():
+            self.__titulo = valor
+        else:
+            print("Erro: Título não pode ser vazio.")
+
+    @property
+    def ano(self):
+        return self.__ano
+
+    @ano.setter
+    def ano(self, valor):
+        if valor and valor > 0:
+            self.__ano = valor
+        else:
+            print("Erro: Ano deve ser maior que 0.")
+
+    @property
+    def disponivel(self):
+        return self.__disponivel
