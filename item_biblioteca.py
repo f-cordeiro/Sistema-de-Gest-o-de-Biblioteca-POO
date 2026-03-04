0e8734f18c9e1a88640e50d1a2c1b2d6e9dcc8b0
Felipe Cordeiro <olifelipecordeiro@gmail.com>
2026-03-04 09:52:10 -0300

    Criação da Classe Itembiblioteca + Encapsulamento

---
+++ item_biblioteca.py
@@ -0,0 +1,6 @@
+class ItemBiblioteca:
+    def __init__(self, codigo, titulo, ano):
+        self.__codigo = codigo
+        self.__titulo = titulo
+        self.__ano = ano
+        self.__disponivel = True
